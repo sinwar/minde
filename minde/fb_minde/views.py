f0eec72fa45e7636d46dc5d1061c86c89cb5be4b
sinwar <premsinwar4@gmail.com>
2016-08-13 21:47:04 +0530

    reponse worked perfectly

--- minde/fb_minde/views.py
+++ minde/fb_minde/views.py
@@ -20,24 +20,9 @@ jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cu
                   """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] }
 
 # Helper function
-def post_facebook_message(fbid, recevied_message):
-    # Remove all punctuations, lower case the text and split it based on space
-    tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
-    joke_text = ''
-    for token in tokens:
-        if token in jokes:
-            joke_text = random.choice(jokes[token])
-            break
-    if not joke_text:
-        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!" 
-
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
-    user_details = requests.get(user_details_url, user_details_params).json() 
-    joke_text = 'Yo ..! ' + joke_text
-                   
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+def post_facebook_message(fbid, recevied_message):           
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAHZAyF2pZAlMBAAXAmutZAkKggoHzIZARtTFmUcLvsqUClJoZCvdUZCggsxBXyibjo1iIoNZBt0szKChpTJDukzb1pBrPNS53y9g0osjocxIWuitXA58VQuzhRGHXXR7tPpeXKIBTZCe5JRPTz4PttNee2ZC8x1N9yUdSUfoKf0wgQZDZD' 
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
@@ -65,8 +50,18 @@ class mindeview(generic.View):
                 # This might be delivery, optin, postback for other events 
                 if 'message' in message:
                     # Print the message to the terminal
-                    pprint(message)    
+                    # pprint(message)    
                     # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
                     # are sent as attachments and must be handled accordingly. 
-                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+
+                    # if images or extra achievements will be added in attachmetns 
+                    try:
+                        sender = message['sender']['id']
+                        if sender != 860649504016574:
+                            pprint(message)
+                        post_facebook_message(sender, message['message']['text'])
+                    except KeyError:
+                        pass
+
+                        
         return HttpResponse()    
